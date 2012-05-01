e916f36c0e3395e5df752e62f516b92d47e075f1
Tomek Meka <tomek@gildia.pl>
2012-05-01 13:04:30 +0200

    addes subjects/categories test

---
+++ test/elibri_categories_test.rb
@@ -0,0 +1,13 @@
+require 'helper'
+
+describe Elibri::ONIX::Release_3_0::ONIXMessage do
+
+  it "should be able to parse categories informations stored in Elibri" do
+    product = load_fixture("onix_categories_example.xml")
+    assert_equal 2, product.subjects.size
+
+    assert_equal "1110", product.subjects[0].code
+    assert_equal "Historia / II Wojna Światowa / Ruch oporu", product.subjects[0].heading_text
+  end
+
+end
