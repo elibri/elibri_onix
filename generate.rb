04051fbb553b7d87510751c5cf7f111fb61b49bb
Piotr Szmielew <p.szmielew@beinformed.pl>
2012-05-11 00:08:41 +0200

    fields

---
+++ generate.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'lib/elibri_onix'
+
+File.open("FIELDS.md", 'w') do |f|
+  f.write("# List of attributes in Elibri_Onix classes\n\n")
+  Elibri::ONIX::Release_3_0.constants.sort.each do |submodule|
+    f.write("## #{submodule}\n")
+    f.write "### Attributes\n" + "Elibri::ONIX::Release_3_0::#{submodule}".constantize::ATTRIBUTES.join(", ")
+    f.write "\n"
+    f.write "### Relations\n" + "Elibri::ONIX::Release_3_0::#{submodule}".constantize::RELATIONS.join(", ") unless "Elibri::ONIX::Release_3_0::#{submodule}".constantize::RELATIONS.empty?
+    f.write "\n\n"
+  end
+end
