caef8dd3df09b91be2d1e880e100db5f5752e4e2
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-01-02 12:58:46 +0100

    when parsing publishing date get fromat from (now deprecated) DateFormat tag, or dateformat attr or use default '00'

--- lib/elibri_onix/onix_3_0/publishing_date.rb
+++ lib/elibri_onix/onix_3_0/publishing_date.rb
@@ -6,26 +6,26 @@ module Elibri
     module Release_3_0
 
       class PublishingDate
-        
+
         #from ONIX documentation:
         #A repeatable group of data elements which together specify a date associated with the publishing of the product.
         #Optional, but a date of publication must be specified either or in <MarketPublishingDetail> (P.25).
         #Other dates related to the publishing of a product can be sent in further repeats.
 
         include HashId
-        
+
         ATTRIBUTES = [
           :role, :format, :date, :parsed
         ]
-        
+
         RELATIONS = []
-        
+
         attr_accessor :role, :format, :date, :to_xml
-        
+
         def initialize(data)
           @to_xml = data.to_s
           @role = data.at_css('PublishingDateRole').try(:text)
-          @format = data.at_css('DateFormat').try(:text)
+          @format = data.at_css('DateFormat').try(:text) || data.at_css('Date')['dateformat'] || '00'
           @date = data.at_css('Date').try(:text)
         end
 
